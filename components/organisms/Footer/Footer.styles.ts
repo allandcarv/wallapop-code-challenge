c2c927b42e9c7fcba35683283c26da13245269b8
Allan Carvalho <allan.carvalho@ffptm2zl0hplvdn.home>
2022-02-24 10:52:54 +0000

    refactor: add contacts to footer

--- components/organisms/Footer/Footer.styles.ts
+++ components/organisms/Footer/Footer.styles.ts
@@ -3,12 +3,19 @@ import styled from 'styled-components';
 export const StyledFooter = styled.footer`
   grid-area: footer;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: var(--blue);
 
   p {
+    margin-bottom: 0.5em;
     color: var(--light-gray);
+
+    strong {
+      font-family: 'Roboto Bold', sans-serif;
+      color: inherit;
+    }
 
     a {
       color: var(--light-gray);
@@ -16,6 +23,23 @@ export const StyledFooter = styled.footer`
 
       &:hover {
         color: var(--gray);
+      }
+    }
+  }
+
+  section {
+    a {
+      & + a {
+        margin-left: 0.5rem;
+      }
+
+      svg {
+        fill: var(--light-gray);
+        transition: fill 0.4s;
+
+        &:hover {
+          fill: var(--gray);
+        }
       }
     }
   }
--- components/organisms/Footer/Footer.tsx
+++ components/organisms/Footer/Footer.tsx
@@ -1,18 +1,42 @@
 import { StyledFooter } from './Footer.styles';
 
+import {
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+  AiOutlineMail,
+} from 'react-icons/ai';
+
 const Footer: React.FC = () => {
   return (
     <StyledFooter>
       <p>
-        Developed by{' '}
+        Developed by <strong>Allan Carvalho</strong>
+      </p>
+      <section>
         <a
           target="_blank"
           href="https://www.linkedin.com/in/allandc/"
           rel="noreferrer"
         >
-          Allan Carvalho
+          <AiOutlineMail size="1.5em" title="Send Email to Allan Carvalho" />
         </a>
-      </p>
+
+        <a
+          target="_blank"
+          href="https://github.com/allandcarv/"
+          rel="noreferrer"
+        >
+          <AiOutlineGithub size="1.5em" title="Allan Carvalho's GitHub" />
+        </a>
+
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/allandc/"
+          rel="noreferrer"
+        >
+          <AiOutlineLinkedin size="1.5em" title="Allan Carvalho's LinkedIn" />
+        </a>
+      </section>
     </StyledFooter>
   );
 };
