1367ec58f5ed2c408ba8a20272ec0a2b522ecfd2
Allan Carvalho <allan.carvalho@ffptm2zl0hplvdn.home>
2022-02-25 12:24:46 +0000

    feat: add favorites button and breakpoints to header

--- components/organisms/Header/Header.styles.ts
+++ components/organisms/Header/Header.styles.ts
@@ -1,13 +1,45 @@
 import styled from 'styled-components';
 
+import BREAKPOINTS from '../../../styles/breakpoints';
+
 export const StyledHeader = styled.header`
   grid-area: header;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   background-color: var(--blue);
 
-  h1 {
-    color: var(--light-gray);
+  section.container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.5rem;
+
+    h1 {
+      font-size: 1.3rem;
+      color: var(--light-gray);
+    }
+
+    button {
+      font-size: 1rem;
+      color: var(--light-gray);
+      border-color: var(--light-gray);
+    }
+
+    @media only screen and (min-width: ${BREAKPOINTS.small}) {
+      max-width: 80%;
+      margin: auto;
+
+      h1 {
+        font-size: 2rem;
+      }
+
+      button {
+        font-size: 1.5rem;
+      }
+    }
+
+    @media only screen and (min-width: ${BREAKPOINTS.large}) {
+      max-width: 60%;
+    }
   }
 `;
--- components/organisms/Header/Header.tsx
+++ components/organisms/Header/Header.tsx
@@ -1,9 +1,22 @@
 import { StyledHeader } from './Header.styles';
 
-const Header: React.FC = () => (
-  <StyledHeader>
-    <h1>ITEM MANAGER</h1>
-  </StyledHeader>
-);
+import { Button } from '../../atoms';
+
+import { useFavorites } from '../../../shared/hooks';
+
+const Header: React.FC = () => {
+  const { getFavorites } = useFavorites();
+
+  const favorites = getFavorites().length;
+
+  return (
+    <StyledHeader>
+      <section className="container">
+        <h1>ITEM MANAGER</h1>
+        <Button customType="outline">Favorites: {favorites}</Button>
+      </section>
+    </StyledHeader>
+  );
+};
 
 export default Header;
